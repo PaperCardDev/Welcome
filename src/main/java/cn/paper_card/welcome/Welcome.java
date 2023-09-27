c356d90f1cbfafe499d97f7c7e5ce2abd506d0d0
Paper99 <1398872296@qq.com>
2023-09-27 18:25:31 +0800

    欢迎萌新提示语。

--- src/main/java/cn/paper_card/welcome/Welcome.java
+++ src/main/java/cn/paper_card/welcome/Welcome.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -13,21 +14,54 @@ import org.jetbrains.annotations.NotNull;
 
 public final class Welcome extends JavaPlugin implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onJoin(@NotNull PlayerJoinEvent event) {
+
         final Player player = event.getPlayer();
-        if (player.hasPlayedBefore()) {
-            event.joinMessage(Component.text()
-                    .append(player.displayName())
-                    .append(Component.space())
-                    .append(Component.text("欢迎回来吖~").color(NamedTextColor.AQUA).decorate(TextDecoration.BOLD))
-                    .build());
-        } else {
+        final long firstPlayed = player.getFirstPlayed();
+        final long delta = System.currentTimeMillis() - firstPlayed;
+
+        final NamedTextColor nameColor = player.isOp() ? NamedTextColor.DARK_RED : NamedTextColor.WHITE;
+
+        final Component prefix;
+
+        // 萌新头衔
+        if (firstPlayed == 0 || delta < 7 * 24 * 60 * 60 * 1000L) {
+            prefix = Component.text("[萌新]").color(NamedTextColor.LIGHT_PURPLE);
+        } else prefix = Component.text("");
+
+        player.displayName(Component.text()
+                .append(prefix)
+                .append(Component.text(" "))
+                .append(Component.text(player.getName()).color(nameColor))
+                .build());
+
+        player.customName(player.displayName());
+
+        // 萌新欢迎语
+        if (firstPlayed == 0 || delta < 24 * 60 * 60 * 1000L) {
+
             event.joinMessage(Component.text()
                     .append(player.displayName())
                     .append(Component.space())
                     .append(Component.text("欢迎萌新~").color(NamedTextColor.LIGHT_PURPLE).decorate(TextDecoration.BOLD))
                     .build());
+
+            if (firstPlayed == 0) {
+                getServer().broadcast(Component.text()
+                        .append(player.displayName())
+                        .append(Component.text(" 这是你第一次进入服务器吖~").color(NamedTextColor.DARK_RED).decorate(TextDecoration.BOLD))
+                        .build());
+            }
+
+        } else {
+
+            event.joinMessage(Component.text()
+                    .append(player.displayName())
+                    .append(Component.space())
+                    .append(Component.text("欢迎回来吖~").color(NamedTextColor.AQUA).decorate(TextDecoration.BOLD))
+                    .build());
+
         }
     }
 
